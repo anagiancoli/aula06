6815776ac0f47f1f1ca4e2789df369b78fa75774
Ana Giancoli <paulagiancoli@gmail.com>
2021-07-23 14:10:53 -0300

    adicao servicos

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+import { I18n } from 'aws-amplify';
+import AmplifyI18n from 'amplify-i18n';
+
+Amplify.configure(config);
+
+const locales = ["en", "fr", "pt-BR"]
+AmplifyI18n.configure(locales)
+I18n.setLanguage("pt-BR")
+
 
 ReactDOM.render(
   <React.StrictMode>
